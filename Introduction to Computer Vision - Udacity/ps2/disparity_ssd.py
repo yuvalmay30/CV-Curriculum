9b09cd4b5804d1b0438ad1001177966565a98886
maymonyu <yuvalmaymon30@gmail.com>
2021-10-29 16:59:57 +0300

    Done

--- Introduction to Computer Vision - Udacity/ps2/disparity_ssd.py
+++ Introduction to Computer Vision - Udacity/ps2/disparity_ssd.py
@@ -1,7 +1,9 @@
+from cv2 import cv2
 import numpy as np
+from tqdm import tqdm
 
 
-def disparity_ssd(source_image, template_image, window_edge_size=2, max_offset=30):
+def disparity_ssd(source_image, template_image, window_edge_size=2, max_offset=10, matching_function=None):
     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
     
     Params:
@@ -17,7 +19,7 @@ def disparity_ssd(source_image, template_image, window_edge_size=2, max_offset=3
     padded_source_image = pad_image_with_zeros(source_image, window_edge_size)
     padded_template_image = pad_image_with_zeros(template_image, window_edge_size)
 
-    for row_index in range(source_image.shape[0]):
+    for row_index in tqdm(range(source_image.shape[0])):
         for column_index in range(source_image.shape[1]):
             disparity_image[row_index, column_index] = \
                 calculate_disparity_for_pixel(row_index + half_window_size,
@@ -25,22 +27,30 @@ def disparity_ssd(source_image, template_image, window_edge_size=2, max_offset=3
                                               window_edge_size,
                                               padded_source_image,
                                               padded_template_image,
-                                              max_offset)
+                                              max_offset,
+                                              matching_function)
 
     return disparity_image
 
 
-def calculate_disparity_for_pixel(row_index, column_index, window_edge_size, source_image, template_image, max_offset):
+def calculate_disparity_for_pixel(row_index, column_index, window_edge_size,
+                                  source_image, template_image, max_offset, matching_function):
     half_window_size = int(window_edge_size / 2)
     minimal_ssd, minimal_ssd_index = None, None
 
     source_window = get_window_around_pixel(row_index, column_index, window_edge_size, source_image)
 
-    for j in range(template_image.shape[1] - window_edge_size):
-        window_column_index = j + half_window_size
-        template_window = get_window_around_pixel(row_index, window_column_index, window_edge_size, template_image)
+    left_bound_window_index = max(column_index - max_offset, half_window_size)
+    right_bound_window_index = min(column_index + max_offset, template_image.shape[1] - half_window_size)
 
-        ssd = calculate_ssd(source_window, template_window)
+    for window_column_index in range(left_bound_window_index, right_bound_window_index + 1):
+        template_window = get_window_around_pixel(row_index, window_column_index, window_edge_size, template_image)
+        ssd = None
+
+        if matching_function in [None, 'ssd']:
+            ssd = calculate_ssd(source_window, template_window)
+        else:
+            ssd = cv2.matchTemplate(source_window, template_window, cv2.TM_CCOEFF_NORMED)
 
         if minimal_ssd is None or ssd < minimal_ssd:
             minimal_ssd = ssd
@@ -72,4 +82,5 @@ def pad_image_with_zeros(image, pad_size):
 
     padded_image[half_pad_size: num_of_rows - half_pad_size, half_pad_size: num_of_columns - half_pad_size] = image
 
+    padded_image = padded_image.astype(np.uint8)
     return padded_image
--- Introduction to Computer Vision - Udacity/ps2/ps2.py
+++ Introduction to Computer Vision - Udacity/ps2/ps2.py
@@ -14,56 +14,94 @@ def question1():
     L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
     R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
 
-    D_L = disparity_ssd(L, R)
+    D_L = disparity_ssd(L, R, 8, 30)
     normalized_d_l = normalize_disparity_image(D_L)
 
-    D_R = disparity_ssd(R, L)
+    D_R = disparity_ssd(R, L, 8, 30)
     normalized_d_r = normalize_disparity_image(D_R)
 
     cv2.imwrite(os.path.join('output', 'ps2-1-a-1.png'), normalized_d_l)
     cv2.imwrite(os.path.join('output', 'ps2-1-a-2.png'), normalized_d_r)
 
 
-def ps2_question2():
+def question2():
     L = cv2.imread(os.path.join('input', 'pair1-L.png'))
     L = cv2.cvtColor(L, cv2.COLOR_BGR2GRAY)
 
     R = cv2.imread(os.path.join('input', 'pair1-R.png'))
     R = cv2.cvtColor(R, cv2.COLOR_BGR2GRAY)
 
-    D_L = disparity_ssd(L, R)
-    normalized_d_l = normalize_disparity_image(D_L)
+    D_L = disparity_ssd(L, R, 4, 80)
+    normalized_d_l = np.abs(D_L)
+    normalized_d_l = normalize_disparity_image(normalized_d_l)
 
     cv2.imwrite(os.path.join('output', 'ps2-2-a-1.png'), normalized_d_l)
+
     print('Done processing disparity L')
 
-    D_R = disparity_ssd(R, L)
-    normalized_d_r = normalize_disparity_image(D_R)
+    D_R = disparity_ssd(R, L, 4, 80)
+    normalized_d_r = np.abs(D_R)
+    normalized_d_r = normalize_disparity_image(normalized_d_r)
 
     cv2.imwrite(os.path.join('output', 'ps2-2-a-2.png'), normalized_d_r)
 
 
-def ps2_question3():
+def question3():
     rng = np.random.default_rng()
 
     L = cv2.imread(os.path.join('input', 'pair1-L.png'))
     L = cv2.cvtColor(L, cv2.COLOR_BGR2GRAY)
-    L += rng.normal(0, 100, L.shape)
+    L = L.astype(float)
+    L += rng.normal(0, 20, L.shape)
+    L = L.astype(np.uint8)
 
     R = cv2.imread(os.path.join('input', 'pair1-R.png'))
     R = cv2.cvtColor(R, cv2.COLOR_BGR2GRAY)
-    R += rng.normal(0, 100, L.shape)
+    R = R.astype(float)
+    R += rng.normal(0, 20, R.shape)
+    R = R.astype(np.uint8)
 
-    D_L = disparity_ssd(L, R)
-    normalized_d_l = normalize_disparity_image(D_L)
+    D_L = disparity_ssd(L, R, 4, 80)
+    normalized_d_l = np.abs(D_L)
+    normalized_d_l = normalize_disparity_image(normalized_d_l)
 
     cv2.imwrite(os.path.join('output', 'ps2-3-a-1.png'), normalized_d_l)
     print('Done processing disparity L')
 
-    D_R = disparity_ssd(R, L)
-    normalized_d_r = normalize_disparity_image(D_R)
+    D_R = disparity_ssd(R, L, 4, 80)
+    normalized_d_r = np.abs(D_R)
+    normalized_d_r = normalize_disparity_image(normalized_d_r)
 
     cv2.imwrite(os.path.join('output', 'ps2-3-a-2.png'), normalized_d_r)
 
 
-ps2_question2()
+def question4():
+    # rng = np.random.default_rng()
+
+    L = cv2.imread(os.path.join('input', 'pair1-L.png'))
+    L = cv2.cvtColor(L, cv2.COLOR_BGR2GRAY)
+    # L = L.astype(float)
+    # L += rng.normal(0, 20, L.shape)
+    L = L.astype(np.uint8)
+
+    R = cv2.imread(os.path.join('input', 'pair1-R.png'))
+    R = cv2.cvtColor(R, cv2.COLOR_BGR2GRAY)
+    # R = R.astype(float)
+    # R += rng.normal(0, 20, R.shape)
+    R = R.astype(np.uint8)
+
+    D_L = disparity_ssd(L, R, 8, 80, 'ccoeff_normed')
+    normalized_d_l = np.abs(D_L)
+    normalized_d_l = normalize_disparity_image(normalized_d_l)
+
+    cv2.imwrite(os.path.join('output', 'ps2-4-a-1.png'), normalized_d_l)
+    print('Done processing disparity L')
+
+    # D_R = disparity_ssd(R, L, 4, 80, 'ccoeff_normed')
+    # normalized_d_r = np.abs(D_R)
+    # normalized_d_r = normalize_disparity_image(normalized_d_r)
+    #
+    # cv2.imwrite(os.path.join('output', 'ps2-4-a-2.png'), normalized_d_r)
+
+
+question4()
